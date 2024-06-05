49902460303d0ad0698f170f90e1f78245c7e5a5
nadiakhairiya <nadiakhairiya9@gmail.com>
2024-06-05 15:40:10 +0800

    CRUD PRODI

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -30,10 +30,45 @@ class ProdiController extends Controller
             [
                 'nama_prodi' => 'required|unique:prodi|max:255'
             ],
+            [
+                'nama_prodi.required' => 'Nama Prodi harus diisi',
+                'nama_prodi.unique' => 'Nama Prodi sudah ada',
+                'nama_prodi.max' => 'Nama Prodi Maksimal 255 karakter'
+            ]
         );
             Prodi::create($validateData);
             return redirect ('/prodi');
     }
+
+    public function edit(String $id)
+    {
+        $data = ['nama' => "Nadia", 'foto' =>'E020322106.jpg'];
+        $prodi = Prodi::find($id);
+        return view('prodi.edit', compact(['data', 'prodi']));  
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $validateData = $request->validate(
+            [
+                'nama_prodi' => 'required|unique:prodi|max:255'
+            ],
+            [
+                'nama_prodi.required' => 'Nama Prodi harus diisi',
+                'nama_prodi.unique' => 'Nama Prodi sudah ada',
+                'nama_prodi.max' => 'Nama Prodi Maksimal 255 karakter'
+            ]
+            );
+            Prodi::where('id', $id)->update($validateData);
+            return redirect('/prodi');
+    }
+
+    public function destroy(string $id)
+    {
+        Prodi::destroy($id);
+        return redirect('/prodi');
+    }
 }
+
 
 
